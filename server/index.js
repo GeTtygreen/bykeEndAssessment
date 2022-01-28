832e9ab2135a04aadae6286f9d21998dd924c9ac
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-01-28 10:22:50 -0800

    not quite done buttttt

--- server/index.js
+++ server/index.js
@@ -8,6 +8,27 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+const getData = () =>{
+axios.get("https://thesimpsonsquoteapi.glitch.me/quotes")
+    .then(response=>{
+      console.log(response)
+    })
+}
+app.get('/api/fortune',(req, res) => {
+  const fortunes =[
+    "A gambler not only will lose what he has, but also will lose what he doesn’t have.",
+    "A light heart carries you through all the hard times.",
+    "Bird in the hand is worth 2 in the bush",
+    "The grass isnt all ways greener on the other side , just be happy where you reside ",
+    "whenever someone txts you and not call you , its normally somthing they couldnt have the heart to tell you verbally "
+  ]
+
+  let randomIndex = Math.floor(Math.random()* fortunes.length)
+  let randomFortune = fortunes[randomIndex]
+
+  res.status(200).send(randomFortune)
+})
+
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
